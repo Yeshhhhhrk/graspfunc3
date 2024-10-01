b7cafb15accb060dc4d7b2282d87ff1b16e98917
Yeshhhhhrk <yeshhhhrk@gmail.com>
2024-10-01 14:20:02 -0400

    Update generate_sc_files.py

--- src/pre-processing/generate_sc_files.py
+++ src/pre-processing/generate_sc_files.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -8,27 +8,32 @@ import sys
 # NOTE: This requires qhull files as input.
 
 # USAGE:
-#  $ python __FILE__.py <pdb-files.dat>
+#  $ python __FILE__.py <pdb-files.dat>
 
 def main():
   pdbFile = open(sys.argv[1], 'r')
-  pdbs = pdbFile.readlines();
+  pdbs = pdbFile.readlines()
   pdbFile.close()
-  cleanUp = lambda x: x.split('\n')[0]
-  qhullify = lambda x: x.replace('.pdb', '.qhull')
-  pdbs = map(cleanUp, pdbs)
-  qhulls = map(qhullify, pdbs)
+
+  def clean_up(x):
+    return x.split('\n')[0]
+
+  def qhullify(x):
+    return x.replace('.pdb', '.qhull')
+
+  pdbs = list(map(clean_up, pdbs))  # Convert map object to list
+  qhulls = list(map(qhullify, pdbs))  # Convert map object to list
+
   for q in qhulls:
-    if os.path.isfile(q) and q[-6:]=='.qhull':
-      print 'Processing: {0}'.format(q)
+    if os.path.isfile(q) and q[-6:] == '.qhull':
+      print('Processing: {0}'.format(q))
       generate_sc(os.path.dirname(q), os.path.basename(q))
 
 def generate_sc(qhullPath, qhull):
   qhullPath = "."
   qhullFile = qhullPath + '/' + qhull
-  command = "qhull TI \""\
-      + qhullFile + "\" d QJ Ft | grep -v \"\\.\" | cut -d\' \' -f2,3,4,5 >\"" + qhullPath + "/" + qhull[:-6] + ".sc\""
-  print "Command: ", command
+  command = "qhull TI \"" + qhullFile + "\" d QJ Ft | grep -v \"\\.\" | cut -d\' \' -f2,3,4,5 >\"" + qhullPath + "/" + qhull[:-6] + ".sc\""
+  print("Command: ", command)
   os.system(command)
 
 main()

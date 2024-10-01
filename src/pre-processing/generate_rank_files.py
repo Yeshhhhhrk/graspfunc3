8d24428d3040083f67dfbf2f971edb884529ec87
Yeshhhhhrk <yeshhhhrk@gmail.com>
2024-10-01 14:14:38 -0400

    Update generate_rank_files.py

--- src/pre-processing/generate_rank_files.py
+++ src/pre-processing/generate_rank_files.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -7,16 +7,17 @@ import re
 # For generating *.rank files for all proteins
 
 # USAGE:
-#  $ python __FILE__.py <rank-files.dat>
+#  $ python __FILE__.py <rank-files.dat>
 
 # This assumes that there is a rank_files.dat in the
 # current directory containing a list of the original
 # POOL-generated rank files.
 
 def main():
-  for l in open(sys.argv[1], 'r'):
-    if l == "": continue
-    generate_rank_file(l.split('\n')[0], l[3])
+  for line in open(sys.argv[1], 'r'):
+    if not line:
+      continue
+    generate_rank_file(line.split('\n')[0], line[3])
 
 
 def generate_rank_file(fl, n):
@@ -24,15 +25,15 @@ def generate_rank_file(fl, n):
   lines = f.readlines()
   f.close()
 
-  newdir="."
+  newdir = "."
   if not os.path.isdir(newdir):
     os.mkdir(newdir)
-  f = open(newdir+'/'+fl.split('/')[-1].split('.')[0]+'.rank', 'w')
-  
-  for l in lines:
+  f = open(newdir + '/' + fl.split('/')[-1].split('.')[0] + '.rank', 'w')
+
+  for line in lines:
     #if count > 200:
-    #    break
-    strs = re.split("\s+", l)
+    #  break
+    strs = re.split("\s+", line)
     f.write(strs[1] + " " + strs[2] + "\n")
   f.close()
 
